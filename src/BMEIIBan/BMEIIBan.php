a68aab52561de350e19d3960bd30f0c1f4c91b7c
Buda Nemeth <nemeth.buda@maszk.net>
2018-03-13 23:39:32 +0100

    getbanlist

---
+++ src/BMEIIBan/BMEIIBan.php
@@ -0,0 +1,21 @@
+<?php
+
+use Ixudra/Curl;
+
+class BMEIIBan
+{
+    public function getbanlist(){
+      $list = Curl::to('http://net.bme.hu/filter/')->get();
+      $list = explode('IDE CSAK A TABLAZAT SORAIT',$list)[1];
+      $list = explode(PHP_EOL,$list);
+      $banned = collect();
+      foreach ($list as $row) {
+        if (preg_match('/martos.bme.hu/',$row)) {
+          $row = explode('<td>',$row)[1];
+          $row = explode(' (',$row)[0];
+          $banned->push($row);
+        }
+      }
+      return $banned;
+    }
+}
